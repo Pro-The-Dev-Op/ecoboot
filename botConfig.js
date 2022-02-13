d07ca13e01812c454cdc57f7dc3629e7686f27df
Pro-The-Dev-Op <raghaov@gmail.com>
2022-02-13 14:10:05 +0530

    make it better

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "OTQxMDEyNzQ2MTQzNjcwMzk0.YgPwaQ.gKDFFFcBfxm3vmzcw4y-RuURGGo",
+    prefix: "p.",
+    admins: [
+        ""
+],
+    debug: true,
+    countChannel: "countChannelID"
+};
---
+++ events/ready.js
@@ -0,0 +1,4 @@
+module.exports = (client) => {
+    console.log(`${client.user.tag} is online!`);
+  client.user.setActivity("Coded by Raghaov Sharma/Pro Gamer website is https://raghaov.com");
+};
